52b7e8694c98c5398c8948cffbb9ac12080f06c0
John-Michael Riley <johnmichael.w.riley@gmail.com>
2020-11-05 13:34:02 -0500

    Add session configuration

---
+++ config/database.js
@@ -0,0 +1,10 @@
+module.exports = {
+  development: {
+    username: 'root',
+    password: null,
+    database: 'database_development',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+  },
+};
